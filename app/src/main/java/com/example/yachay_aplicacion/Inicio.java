3f4bdfb054514aa369a3e86f3e10f430ffcc4e31
htr999999999 <chris_acc@outlook.com>
2023-07-02 20:09:33 -0500

    kidna final

--- app/src/main/java/com/example/yachay_aplicacion/Inicio.java
+++ app/src/main/java/com/example/yachay_aplicacion/Inicio.java
@@ -1,15 +1,19 @@
 package com.example.yachay_aplicacion;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
-public class Inicio extends Fragment {
+public class Inicio extends Fragment implements View.OnClickListener {
+    CardView modulo1,modulo2,modulo3,modulo4,modulo5,modulo6;
 
 
     public Inicio() {
@@ -20,7 +24,53 @@ public class Inicio extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        View v=inflater.inflate(R.layout.fragment_inicio, container, false);
+        modulo1 = v.findViewById(R.id.activi1);
+        modulo1.setOnClickListener(this);
+        modulo2 = v.findViewById(R.id.activi2);
+        modulo2.setOnClickListener(this);
+        modulo3 = v.findViewById(R.id.activi3);
+        modulo3.setOnClickListener(this);
+        modulo4 = v.findViewById(R.id.activi4);
+        modulo4.setOnClickListener(this);
+        modulo5 = v.findViewById(R.id.activi5);
+        modulo5.setOnClickListener(this);
+        modulo6 = v.findViewById(R.id.activi6);
+        modulo6.setOnClickListener(this);
+
+        return v;
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activi1:
+                Intent intent = new Intent(getContext(), Modulo1.class);
+                startActivity(intent);
+                break;
+            case R.id.activi2:
+                Intent intent1 = new Intent(getContext(), Modulo2.class);
+                startActivity(intent1);
+                break;
+            case R.id.activi3:
+                Intent intent2 = new Intent(getContext(), Modulo3.class);
+                startActivity(intent2);
+                break;
+            case R.id.activi4:
+                Intent intent3 = new Intent(getContext(), Modulo4.class);
+                startActivity(intent3);
+                break;
+            case R.id.activi5:
+                Intent intent4 = new Intent(getContext(), Modulo5.class);
+                startActivity(intent4);
+                break;
+            case R.id.activi6:
+                Intent intent5 = new Intent(getContext(), Modulo6.class);
+                startActivity(intent5);
+                break;
+        }
+
+
+    }
 }
